f81c5dc4bf9692a927a672901e29728e09994bdb
Brien Mizell <40799762+brienmizell@users.noreply.github.com>
2019-02-26 00:01:42 -0500

    Finished plush minus algorithm.

--- hr-plusMinus/index.js
+++ hr-plusMinus/index.js
@@ -11,3 +11,22 @@
 // 0.500000
 // 0.333333
 // 0.166667
+
+function plusMinus(arr) {
+  let positive = 0;
+  let negative = 0;
+  let zero = 0;
+  let denominator = arr.length;
+  arr.forEach(n => {
+    if (n > 0) {
+      positive++;
+    } else if (n < 0) {
+      negative++;
+    } else {
+      zero++;
+    }
+  });
+  console.log((positive / denominator).toFixed(6));
+  console.log((negative / denominator).toFixed(6));
+  console.log((zero / denominator).toFixed(6));
+}
