11b90a6fc9df36e4d85b0a36cabfa70a0d5227ca
Brien Mizell <40799762+brienmizell@users.noreply.github.com>
2019-02-25 23:50:25 -0500

    Starting Plus Minus.

---
+++ hr-plusMinus/index.js
@@ -0,0 +1,13 @@
+// Given an array of integers, calculate the fractions
+// of its elements that are positive, negative, and are zeros.
+// Print the decimal value of each fraction on a new line rounded to six decimals.
+
+// Sample Input
+
+// [-4, 3, -9, 0, 4, 1]
+
+// Sample Output
+
+// 0.500000
+// 0.333333
+// 0.166667
