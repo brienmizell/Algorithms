dcabdb5e37973f135feda6cc4d8efa89c2e32e45
Brien Mizell <40799762+brienmizell@users.noreply.github.com>
2019-02-26 14:33:46 -0500

    Finished staircase algorithm.:

--- hr-staircase/index.js
+++ hr-staircase/index.js
@@ -7,4 +7,12 @@
 
 // Print a staircase of size n using # symbols and spaces.
 
-function staircase(n) {}
+function staircase(n) {
+  let numberSpaces = n - 1;
+  for (let i = 0; i < n; i++) {
+    let hash = "#";
+    let mySpaces = " ".repeat(numberSpaces);
+    console.log(mySpaces + hash.repeat(i + 1));
+    numberSpaces--;
+  }
+}
