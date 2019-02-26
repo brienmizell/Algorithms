94c336ddfebe083daaba60d1ca6600476a388dd2
Brien Mizell <40799762+brienmizell@users.noreply.github.com>
2019-02-25 22:48:58 -0500

    Finished diagonal matrix difference algorithm.

--- hr-squareMatrix/index.js
+++ hr-squareMatrix/index.js
@@ -10,3 +10,16 @@
 // The left-to-right diagonal = 1 + 5 + 9. The
 // right to left diagonal = 9 + 5 + 3. Their
 // absolute difference is |15 - 17| = 2.
+function diagonalDifference(arr) {
+  let backSlash = 0;
+  let forwardSlash = 0;
+  let length = arr.length - 1;
+  console.log(length);
+
+  for (let i = 0; i < arr.length; i++) {
+    backSlash += arr[i][i];
+    forwardSlash += arr[i][length];
+    length--;
+  }
+  return Math.abs(backSlash - forwardSlash);
+}
